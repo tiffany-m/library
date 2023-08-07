73a52f16026fec1f135570ad99fba1d2c84c8c0b
Tiffany M <67019104+tiffany-m@users.noreply.github.co>
2023-08-07 11:29:16 -0400

    Add loop to display books in myLibrary on screen

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+let libraryContainer = document.getElementById('library-container')
+
 let myLibrary = [];
 
 function Book(author, title, numPages, read) {
@@ -14,6 +16,14 @@ myLibrary.push(book1);
 myLibrary.push(book2);
 
 console.log(myLibrary);
+
+let book = document.createElement("div");
+for (let i = 0; i < myLibrary.length; i++) {
+    let book = document.createElement("div");
+    book.textContent = `Author: ${myLibrary.author}, Title: ${myLibrary[i].title}, Number of Pages: ${myLibrary[i].numPages}, Read: ${myLibrary[i].read}`
+    libraryContainer.appendChild(book);
+}
+
 
 function addBookToLibrary() {
     // do stuff here

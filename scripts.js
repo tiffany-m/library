78f82dca61c3b677fcfcf1e544c792e7355340bc
Tiffany M <67019104+tiffany-m@users.noreply.github.co>
2023-08-15 11:11:12 -0400

    Redid creating new book and adding to library array

--- scripts.js
+++ scripts.js
@@ -1,11 +1,10 @@
 const libraryContainer = document.getElementById('library-container');
 const newBookBtn = document.getElementById('new-book-btn');
 let data = document.querySelectorAll('input');
-let newData = [];
-// const formAuthor = document.getElementById('author');
-// const formTitle = document.getElementById('title');
-// const formNumPages = document.getElementById('num-pages');
-// const formRead = document.getElementById('read');
+const formAuthor = document.getElementById('author');
+const formTitle = document.getElementById('title');
+const formNumPages = document.getElementById('num-pages');
+const formRead = document.getElementById('read');
 
 let myLibrary = [];
 
@@ -23,28 +22,48 @@ myLibrary.push(book1);
 myLibrary.push(book2);
 
 let book = document.createElement("div");
-function addBookToLibrary() {
+function displayBooks() {
     for (let i = 0; i < myLibrary.length; i++) {
-        book = document.createElement("div");
         book.innerHTML = `
-    ${i + 1}. <br>
-    Author: ${myLibrary[i].author}  <br>
-    Title: ${myLibrary[i].title} <br>
-    Number of Pages: ${myLibrary[i].numPages} <br>
-    Read: ${myLibrary[i].read}`
+            ${i + 1}. <br>
+            Author: ${myLibrary[i].author}  <br>
+            Title: ${myLibrary[i].title} <br>
+            Number of Pages: ${myLibrary[i].numPages} <br>
+            Read: ${myLibrary[i].read}`
         libraryContainer.appendChild(book);
     }
 }
 
 newBookBtn.addEventListener("click", (e) => {
-    let book = []
-    e.preventDefault();
-    newData = Array.from(data)
-    book = Book(newData[0].value, newData[1], newData[2], newData[3])
-    console.log(newData)
-    myLibrary.push(book);
-    addBookToLibrary();
+    e.preventDefault(); // prevent form from submitting
+    console.log(formAuthor.value, formTitle.value, formNumPages.value)
+    let bookInfo = new Book(`${formAuthor.value}`, `${formTitle.value}`, `${formNumPages.value}`, `${formRead.value}`);
+    myLibrary.push(bookInfo);
+    console.log(myLibrary)
+    displayBooks();
+
 })
 
 
-console.log(data[0].textContent)
+
+
+// let data = document.querySelectorAll('input');
+// let newData = [];
+
+// let book = document.createElement("div");
+// function addBookToLibrary() {
+//     for (let i = 0; i < myLibrary.length; i++) {
+//         book = document.createElement("div");
+//         book.innerHTML = `
+//     ${i + 1}. <br>
+//     Author: ${myLibrary[i].author}  <br>
+//     Title: ${myLibrary[i].title} <br>
+//     Number of Pages: ${myLibrary[i].numPages} <br>
+//     Read: ${myLibrary[i].read}`
+//         libraryContainer.appendChild(book);
+//     }
+// }
+
+
+
+// console.log(data[0].textContent)
